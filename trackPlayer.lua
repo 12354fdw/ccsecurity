befbc49b1aa10feecb8b2de0ab155aade3552942
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-24 19:18:42 +0700

    Add files via upload

---
+++ trackPlayer.lua
@@ -0,0 +1,53 @@
+local playerDetec = peripheral.find("playerDetector")
+
+if not playerDetec then
+    print("Please connect a player Dectector")
+    return
+end
+
+local pos = {
+    x=0,
+    y=0,
+    z=0
+} -- self position
+
+local mon = peripheral.find("monitor")
+if mon then
+    term.redirect(mon)
+end
+
+local tracking = {}
+local data = {}
+
+local w,h = term.getSize()
+
+function render()
+    local bg = window.create(term.current(),1,2,w,h)
+    bg.setBackgroundColor(colors.white)
+    bg.setTextColor(colors.lightGray)
+    local title = window.create(term.current(),1,1,w,1)
+    title.setBackgroundColor(colors.gray)
+    title.setTextColor(colors.white)
+    bg.clear()
+    title.clear()
+    title.write("trackPlayer [TEST]")
+    local pos = 2
+    for i,v in pairs(data) do
+        bg.setCursorPos(1,pos)
+        bg.write(i..": "..v.x..", "..v.y..", "..v.z..", dimen: "..v.dimension..", dist: "..string.format("%.2f",v.distance))
+        pos = pos + 1
+    end
+end
+
+while true do
+    for i,v in pairs(tracking) do
+        if not data[v] then data[v] = {} end
+        local td = playerDetec.getPlayerPos(v)
+        data[v] = td
+        local dx,dy,dz = pos.x-td.x,pos.y-td.y,pos.z-td.z
+        local d = math.sqrt(dx^2+dy^2+dz^2)
+        data[v].distance = d
+    end
+    render()
+    sleep(0.1)
+end
