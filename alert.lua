42f549ca15d143bebecdce3567aa230e3fe386f3
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-24 20:09:15 +0700

    Update alert.lua

--- alert.lua
+++ alert.lua
@@ -3,16 +3,18 @@ local args = {...}
 local type = args[1]
 local desc = args[2]
 
-local mons = {peripheral.find("montior")}
+local mons = {peripheral.find("monitor")}
 
 for i,mon in pairs(mons) do
     local w,h = mon.getSize()
     mon.setBackgroundColor(colors[type])
     mon.clear()
     mon.setTextScale(3)
+    w,h = mon.getSize()
     mon.setCursorPos(w/2,h/2)
     mon.write("CODE "..string.upper(type))
     mon.setTextScale(1)
+    w,h = mon.getSize()
     mon.setCursorPos((w/2)-5,h/2+1)
     mon.write(desc)
 end
